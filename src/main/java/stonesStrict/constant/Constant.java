e6aa9ac16b2c59b3f82e02c46e789c29ac93570c
yanlei <897356576@qq.com>
2019-01-11 16:11:04 +0800

    java api配置zookeeper集群调用

--- src/main/java/stonesStrict/constant/Constant.java
+++ src/main/java/stonesStrict/constant/Constant.java
@@ -4,6 +4,7 @@ public class Constant {
 
     //zookeeper服务地址
     public static final String ZK_HOST = "127.0.0.1:2181";
+    //public static final String ZK_HOST = "10.17.130.86:2181,10.17.130.87:2181,10.17.130.87:2182"; //集群配置
 
     //Curator锁根节点
     public static final String CURATOR_ROOT = "curator";
